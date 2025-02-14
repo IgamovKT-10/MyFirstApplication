e0d631b696ae14c520d4dbac6cf1dbdfdb2023e8
IgamovKT-10 <jumabekigamov@gmail.com>
2025-02-14 16:21:18 +0500

    bo'ldi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import android.health.connect.datatypes.SleepSessionRecord
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +21,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            val input = editText.text.toString()
+
+            val result = when (input) {
+                "apple" -> "You entered Apple!"
+                "banana" -> "You entered Banana!"
+                "cherry" -> "You entered Cherry!"
+                else -> "Unknown fruit"
+            }
+
+            // Natijani TextViewga chiqarish
+            textView.text = result
+        }
     }
 }
